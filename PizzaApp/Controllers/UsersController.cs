a5040cf743c4d662c6cb027f5f4c0999ed931227
savely.volodkov@gmail.com <savely.volodkov@gmail.com>
2022-05-15 01:58:10 +0300

    UsersController added

---
+++ PizzaApp/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Data;
+using PizzaApp.Entities;
+
+namespace PizzaApp.Controllers;
+
+public class UsersController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context )
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
--- PizzaApp/Program.cs
+++ PizzaApp/Program.cs
@@ -13,8 +13,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
